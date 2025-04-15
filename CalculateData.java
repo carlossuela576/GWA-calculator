904d90adf4496e2771df1a20fc759327686dd853
Carlos Miguel Marquez Suela <126050848+carlossuela576@users.noreply.github.com>
2025-04-15 23:40:45 +0700

    Create CalculateData.java

---
+++ CalculateData.java
@@ -0,0 +1,22 @@
+public class CalculateData extends ConvertData {
+    double totalWGP = 0;
+    double totalCU = 0;
+    
+    double [] WGP = new double[userCount];
+
+    public void calculateData(){
+        for (int i = 0; i < userCount; i++) {
+            WGP[i] = gradePoint[i] * creditUnits[i];
+        }
+
+        for (int i = 0; i < userCount; i++) {
+            WGP[i] += totalWGP;
+            creditUnits[i] += totalCU;
+        }
+    }
+
+    public double finalGWA(){
+        double GWA = totalWGP / totalCU;
+        return GWA;
+    }
+}
